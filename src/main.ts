6709b4c0d9ce9bd6e5b4703fcf19b458b2db092a
Igor_Fedyukin <igor_fedyukin@epam.com>
2018-07-06 23:47:19 +0300

    Fix prod version for heroku

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from 'app/app.module';
-import { config } from 'config';
+import { AppModule } from './app/app.module';
+import { config } from './config';
 import { accessChecker } from './middlewares';
 
 async function bootstrap() {
